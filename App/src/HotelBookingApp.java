ce91e2a4e14707ec5de8a2bae23bccf36b3f48c1
Pranees-Yuvraj <pr05032007@gmail.com>
2026-03-16 08:39:36 +0530

    Initial Commit UC1

---
+++ App/src/HotelBookingApp.java
@@ -0,0 +1,12 @@
+public class HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("=====================================");
+        System.out.println("        HOTEL BOOKING SYSTEM         ");
+        System.out.println("=====================================");
+        System.out.println("Application Name : Hotel Booking App");
+        System.out.println("Version          : 1.0");
+        System.out.println("Status           : Application Started");
+        System.out.println("=====================================");
+
+    }
+}
